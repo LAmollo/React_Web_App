af685274735b0c9920bce15f8c5f873c5105f180
LAmollo <lawinyamuga@gmail.com>
2024-07-19 17:22:39 -0700

    axiosinstance

--- front-end/src/axiosInstance.js
+++ front-end/src/axiosInstance.js
@@ -1,1 +1,13 @@
-git 
+// frontend/src/axiosInstance.js
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: 'http://localhost:3000/api', // Adjust baseURL based on your backend URL
+});
+
+let userData = JSON.parse(localStorage.getItem('userData'));
+let token = userData ? userData.token : '';
+
+instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+export default instance;
